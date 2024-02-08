2646670ea3f86bdad5196e8a3760c2a9acad4640
tufusa <tufusa74@gmail.com>
2024-02-08 16:19:15 +0900

    fix(ui): スタイリング

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import { useZxing, Result } from "react-zxing";
 import styles from "./App.module.css";
 
 export const App = () => {
-  const [qrContent, setQrContent] = useState<string>("");
+  const [qrContent, setQrContent] = useState<string | null>();
   const [auth, setAuth] = useState(false);
   const onDecodeQr = (result: Result) => {
     const base64 = result.getText();
     const content = atob(base64);
     setQrContent(content);
     setAuth(content === import.meta.env.VITE_AUTH_CONTENT);
-    
   };
   const { ref } = useZxing({
     onDecodeResult: onDecodeQr,
@@ -19,8 +18,10 @@ export const App = () => {
   return (
     <main className={styles.main}>
       <video ref={ref} className={styles.video} />
-      <p>Result:{qrContent}</p>
-      <p>Auth:{auth ? "成功" : "失敗"}</p>
+      <div className={styles.status}>
+        <span>Result:{qrContent}</span>
+        <span>Auth:{!qrContent ? "" : auth ? "成功" : "失敗"}</span>
+      </div>
     </main>
   );
 };
